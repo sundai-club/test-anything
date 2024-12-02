8bbd0799095026a4a80aa1e84ffd6b4906535e1e
Artem Lukoyanov <lukartoil@gmail.com>
2024-12-01 21:01:13 -0500

    UPD: packages and dependencies

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules\/undici/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
