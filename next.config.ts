361ed822c19acd7a73df76b2c14842116e6cb090
Artem Lukoyanov <lukartoil@gmail.com>
2024-12-01 20:28:26 -0500

    UPD: migratre to postgres DB

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"]
+    },
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
